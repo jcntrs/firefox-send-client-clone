01cf96858467c7adb71695887013e48bf7109549
Jean C. Contreras <jean.cntrs@gmail.com>
2020-08-09 13:13:07 -0400

    Add new rules

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        backendURL: 'https://firefox-send-server-clone.herokuapp.com',
+        frontendURL: 'https://firefox-send-client-clone.netlify.app'
+    }
+}
+
+/* module.exports = {
+    env: {
+        backendURL: 'http://localhost:4000',
+        frontendURL: 'http://localhost:3000'
+    }
+} */
